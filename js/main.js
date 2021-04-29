2ece7a948d5cfa4ca740e435a2e789a4b9faaabc
Hazem <457440@student.fontys.nl>
2021-04-29 14:05:50 +0200

    small changes for every page
    Favicon
    AOS
    addes js page

---
+++ js/main.js
@@ -0,0 +1,26 @@
+  
+
+        const currentLocation = location.href;
+        const menuItem = document.querySelectorAll('a');
+        const menuLength = menuItem.length
+        for(let i = 0; i<menuLength; i++ ){
+            if(menuItem[i].href === currentLocation){
+                menuItem[i].className = "active"
+            }
+        }
+
+        $(document).ready(function(){
+        $(window).scroll(function(){
+            // sticky navbar on scroll script
+            if(this.scrollY > 20){
+                $('.navbar').addClass("sticky");
+            }else{
+                $('.navbar').removeClass("sticky");
+            }
+            
+        });
+        
+    });
+
+    AOS.init();
+    
